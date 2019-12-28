22bcab0679441203eaaf5db6c494318a8cb0ae77
Anderson Santos <andersonc1981@gmail.com>
2019-12-27 22:12:07 -0300

    Estado e Cidade

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,3 +9,10 @@ insert into produto_categoria(produto_id, categoria_id) values(1, 1);
 insert into produto_categoria(produto_id, categoria_id) values(2, 1);
 insert into produto_categoria(produto_id, categoria_id) values(2, 2);
 insert into produto_categoria(produto_id, categoria_id) values(3, 1);
+
+insert into estado(nome) values('Minas Gerais');
+insert into estado(nome) values('São Paulo');
+
+insert into cidade(nome, estado_id) values('Uberlândia', 1);
+insert into cidade(nome, estado_id) values('São Paulo', 2);
+insert into cidade(nome, estado_id) values('Campinas', 2);
